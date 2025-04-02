be63ff5c93cc6482150b93443e91de373be5db4c
Slocombe <e467830@us.lmco.com>
2025-04-02 12:15:23 -0600

    Modify files
    Modified Main.java class
    moved methods and variables to the main class to allow easier access and create less clutter.
    User_Information.java class has been commented out, same for any instantiations on the main java class.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        User_Information ui1 = new User_Information();
+        // User_Information ui1 = new User_Information();
 
         System.out.print("What is your current rate?: ");
         double rate = scanner.nextDouble();
@@ -21,13 +21,32 @@ public class Main {
         System.out.print("What's the amount you're putting in on top of the  minimum payment?: ");
         double extra = scanner.nextDouble();
 
-        scanner.close();
-        ui1.setYears(years);
-        ui1.setRate(rate);
-        ui1.setBalance(balance);
-        ui1.setPrincipal(principal);
-        ui1.setExtra(extra);
+        double monthlyInterestRate = rate / 1200;
 
-        System.out.println(ui1);
+        double monthlyPayment = principal * monthlyInterestRate / (1 - 1 / Math.pow(1 + monthlyInterestRate, years * 12));
+
+        double extraPayment = monthlyPayment + extra;
+
+        double monthlyInterest = monthlyInterestRate * balance;
+
+        double totalPayment = (monthlyPayment * 12) * years;
+
+        double months = balance / extraPayment;
+
+        double expeditedPayment = (extraPayment * months) + (months * monthlyInterest);
+
+        System.out.printf("This is the principal of the loan: %.2f, this is the current balance $%.2f, this is the rate: %.2f, and this is the life of the loan: %d\n", principal, balance, rate, years);
+        System.out.printf("This is your current monthly payment: $%.2f, this is the monthly interest: $%.2f. Paying an extra $%.2f with your current balance shortens the life of the loan to %.0f months.\n", monthlyPayment, monthlyInterest, extra, months);
+        System.out.printf("This is the amount you will pay over the life of the loan %.2f and if you choose to put in extra, this is how much you will pay %.2f", totalPayment, expeditedPayment);
+
+
+        scanner.close();
+        // ui1.setYears(years);
+        // ui1.setRate(rate);
+        // ui1.setBalance(balance);
+        // ui1.setPrincipal(principal);
+        // ui1.setExtra(extra);
+
+        // System.out.println(ui1);
     }
 }
--- src/User_Information.java
+++ src/User_Information.java
@@ -1,89 +1,90 @@
-public class User_Information {
-    private double rate;
-    private double balance;
-    private double principal;
-    private int years;
-    private double extra;
+// public class User_Information {
+//     private double rate;
+//     private double balance;
+//     private double principal;
+//     private int years;
+//     private double extra;
 
-    public void setExtra(double extra){
-        this.extra = extra;
-    }
+//     public void setExtra(double extra){
+//         this.extra = extra;
+//     }
 
-    public double getExtra(){
-        return extra;
-    }
+//     public double getExtra(){
+//         return extra;
+//     }
 
-    public void setYears(int years){
-        this.years = years;
-    }
+//     public void setYears(int years){
+//         this.years = years;
+//     }
 
-    public int getYears(){
-        return years;
-    }
+//     public int getYears(){
+//         return years;
+//     }
 
-    public void setRate(double rate){
-        this.rate = rate;
-    }
+//     public void setRate(double rate){
+//         this.rate = rate;
+//     }
 
-    public double getRate(){
-        return rate;
-    }
+//     public double getRate(){
+//         return rate;
+//     }
 
-    public void setBalance(double balance){
-        this.balance = balance;
-    }
+//     public void setBalance(double balance){
+//         this.balance = balance;
+//     }
 
-    public double getBalance() {
-        return balance;
-    }
+//     public double getBalance() {
+//         return balance;
+//     }
 
-    public void setPrincipal(double principal) {
-        this.principal = principal;
-    }
+//     public void setPrincipal(double principal) {
+//         this.principal = principal;
+//     }
 
-    public double getPrincipal() {
-        return principal;
-    }
+//     public double getPrincipal() {
+//         return principal;
+//     }
 
-    public double getDailyInterest(){
-        double dailyInterest = (rate / 365) * principal;
-        return dailyInterest;
-    }
+//     public double getDailyInterest(){
+//         double dailyInterest = (rate / 365) * principal;
+//         return dailyInterest;
+//     }
 
-    public double getMinimumPayment() {
-        double monthlyInterestRate = rate / 1200;
-        double minimumPayment = principal * monthlyInterestRate /
-        (1 - 1 / Math.pow(1 + monthlyInterestRate, years * 12));
-        return minimumPayment;
-    }
+//     public double getMinimumPayment() {
+//         double monthlyInterestRate = rate / 1200;
+//         double minimumPayment = principal * monthlyInterestRate /
+//         (1 - 1 / Math.pow(1 + monthlyInterestRate, years * 12));
+//         return minimumPayment;
+//     }
 
-    public double getTotalPayment(){
-        double totalPayment = getMinimumPayment() * 12 * years;
-        return totalPayment;
-    }
+//     public double getTotalPayment(){
+//         double totalPayment = getMinimumPayment() * 12 * years;
+//         return totalPayment;
+//     }
 
-    public double getExtraPayment(){
-        double extraPayment = getMinimumPayment() + extra;
-        double totalMonths = balance / extraPayment;
-        return totalMonths;
-    }
+//     public double getExtraPayment(){
+//         double extraPayment = getMinimumPayment() + extra;
+//         double totalMonths = balance / extraPayment;
+//         return totalMonths;
+//     }
 
-    public double getExpeditedPayment(){
-        double monthlyInterestRate = rate / 1200;
-        double monthlyInterest = monthlyInterestRate * balance;
-        double extraPayment = getMinimumPayment() + extra;
-        double totalMonths = balance / extraPayment;
-        double expeditedPayment = (getMinimumPayment() + extra) * totalMonths + (totalMonths * monthlyInterest);
-        return expeditedPayment;
-    }
+//     public double getExpeditedPayment(){
+//         double monthlyInterestRate = rate / 1200;
+//         double monthlyInterest = monthlyInterestRate * balance;
+//         double extraPayment = getMinimumPayment() + extra;
+//         double totalMonths = balance / extraPayment;
+//         double expeditedPayment = (getMinimumPayment() + extra) * totalMonths + (totalMonths * monthlyInterest);
+//         return expeditedPayment;
+//         }
 
-    @Override
-    public String toString(){
-        return "This is your rate: " + getRate() + "% this is your monthly payment: $"
-                + String.format("%.02f", getMinimumPayment()) + " this is the remaining balance: $" + getBalance() +
-                " and this is the principal: $" + getPrincipal() + " this is the total amount you will pay over the life of the loan: $" 
-                + String.format("%.02f", getTotalPayment()) + ". This is how long it will take you to pay off your loan adding an extra $"
-                + extra + ": " + String.format("%.01f months", getExtraPayment()) + ". This is the total amount you will pay adding an extra $" + extra + " to the loan: $"
-                + String.format("%.02f", getExpeditedPayment());
-    }
-}
+//     @Override
+//     public String toString(){
+//         return "This is your rate: " + getRate() + "% this is your monthly payment: $"
+//                 + String.format("%.02f", getMinimumPayment()) + " this is the remaining balance: $" + getBalance() +
+//                 " and this is the principal: $" + getPrincipal() + " this is the total amount you will pay over the life of the loan: $" 
+//                 + String.format("%.02f", getTotalPayment()) + ". This is how long it will take you to pay off your loan adding an extra $"
+//                 + extra + ": " + String.format("%.01f months", getExtraPayment())
+//                  + ". This is the total amount you will pay adding an extra $" + extra + " to the loan: $"
+//                 + String.format("%.02f", getExpeditedPayment());
+//     }
+// }
